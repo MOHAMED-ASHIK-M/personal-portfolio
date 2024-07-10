1bbb1c127bc946e5484cbf473d5c5592efe114ef
Mohamed Ashik <ashikbashirmse@gmail.com>
2024-07-11 02:38:14 +0530

    deleted comments

--- script.js
+++ script.js
@@ -1,14 +1,4 @@
-/*var tablinks=document.getElementsByClassName("tab-links")
-var tabcontents=document.getElementsByClassName("tab-contents")
-function opentab(tabname){
-    for(tablink of tablinks){
-        tablink.classList.remove("active-link")
-    }
 
-    for(tabcontent of tabcontents){
-        tabcontent.classList.remove("active-skills")
-    }
-}*/
 
 var tablinks=document.getElementsByClassName("tab-links")
 
